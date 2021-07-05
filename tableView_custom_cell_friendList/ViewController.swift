b0187707da5096b29cf02f2e078a71e5038c9318
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-07-05 11:26:42 -0600

    Initial Commit

---
+++ tableView_custom_cell_friendList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableView_custom_cell_friendList
+//
+//  Created by Kien Nguyen on 2021-07-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
